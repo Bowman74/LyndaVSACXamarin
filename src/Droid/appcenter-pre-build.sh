2dda4cedc2c31f71cd12b5eecf39d1a36be59fb8
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-01-17 10:23:28 -0800

    Add Environment variables

---
+++ src/Droid/appcenter-pre-build.sh
@@ -0,0 +1,3 @@
+﻿#!/usr/bin/env bash
+
+sed -i -e "s/\[VASC_API_KEY]/$VSAC_Key/g" ../Constants.cs
--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -3,3 +3,5 @@ if [ "$APPCENTER_BRANCH" == "QA" ];
 then
 /usr/libexec/plistbuddy -c "Set :CFBundleDisplayName VSACXamarin.$APPCENTER_BRANCH" "iOS/Info.plist"
 fi
+
+sed -i -e "s/\[VASC_API_KEY]/$VSAC_Key/g" Constants.cs
