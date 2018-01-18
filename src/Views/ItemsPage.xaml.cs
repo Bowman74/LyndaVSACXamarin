afabe61323acc4025425feda40906c011ee88478
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-01-18 15:59:55 -0800

    Add analytic.

--- src/Views/ItemsPage.xaml.cs
+++ src/Views/ItemsPage.xaml.cs
@@ -20,17 +20,16 @@ namespace VSACXamarin
 
         async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
         {
-            throw new InvalidCastException("Hey we got an error");
-            //var item = args.SelectedItem as Item;
-            //if (item == null)
-            //    return;
+            var item = args.SelectedItem as Item;
+            if (item == null)
+                return;
 
-            //Analytics.TrackEvent("Item Selected", new Dictionary<string, string> {{ "id", item.Id} , {"description", item.Description}});
+            Analytics.TrackEvent("Item Selected", new Dictionary<string, string> {{ "id", item.Id }, {"description", item.Description}});
 
-            //await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
 
-            //// Manually deselect item
-            //ItemsListView.SelectedItem = null;
+            // Manually deselect item
+            ItemsListView.SelectedItem = null;
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
