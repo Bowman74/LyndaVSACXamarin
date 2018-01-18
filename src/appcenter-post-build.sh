83decd7784e783feda6318b2c0a720e3e58b4089
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2018-01-17 16:48:33 -0800

    Add post build shell script

--- src/appcenter-post-build.sh
+++ src/appcenter-post-build.sh
@@ -1,5 +1,5 @@
 ﻿appcenter login --token $VSAC_Token
 
-appcenter test run uitest --app "KevinsVSACClass/XamarinApp-iOS" --devices 73075a0d --app-path "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin/iOS/bin/iPhone/$APPCENTER_BRANCH/VSACXamarin.ios.ipa"  --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin.UITest.ios/bin/release"
+appcenter test run uitest --app "KevinsVSACClass/XamarinApp-iOS" --devices 73075a0d --app-path "$APPCENTER_SOURCE_DIRECTORY/src/ios/bin/iphone/$APPCENTER_BRANCH/VSACXamarin.ios.ipa"  --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/src/VSACXamarin.UITest.Ios/bin/release"
 
 appcenter logout
